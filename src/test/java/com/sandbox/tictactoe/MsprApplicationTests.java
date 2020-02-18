e7b36862d785755e3248460ebef0031e136907a9
jjaurensan <jj.aurensan@gmail.com>
2020-02-18 13:35:19 +0100

    intialisation du projet MSPR EPSI

---
+++ src/test/java/com/sandbox/tictactoe/MsprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
