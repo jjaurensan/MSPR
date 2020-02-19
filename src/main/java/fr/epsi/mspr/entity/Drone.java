893d496a1b2ebe01f844cc6d87d2f4a2244bdcae
Cédric <cedric_avril_pro@hotmail.fr>
2020-02-19 11:38:00 +0100

    premier push de tests

--- src/main/java/fr/epsi/mspr/entity/Drone.java
+++ src/main/java/fr/epsi/mspr/entity/Drone.java
@@ -1,5 +1,63 @@
 package fr.epsi.mspr.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Drone {
 
+	@Id
+	private long id;
+	private String nom;
+	private int posX;
+	private int posY;
+	private int posZ;
+	private Etat etat;
+	
+	public Drone(String nom, int posX, int posY, int posZ, Etat etat) {
+		this.nom = nom;
+		this.posX = posX;
+		this.posY = posY;
+		this.posZ = posZ;
+		this.etat = etat;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public int getPosZ() {
+		return posZ;
+	}
+	public void setPosZ(int posZ) {
+		this.posZ = posZ;
+	}
+	public Etat getEtat() {
+		return etat;
+	}
+	public void setEtat(Etat etat) {
+		this.etat = etat;
+	}
+	
+	
 }
--- src/test/java/fr/epsi/mspr/service/DroneServiceTest.java
+++ src/test/java/fr/epsi/mspr/service/DroneServiceTest.java
@@ -2,13 +2,36 @@ package fr.epsi.mspr.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.epsi.mspr.entity.Drone;
+import fr.epsi.mspr.entity.Etat;
+import fr.epsi.mspr.repository.DroneRepository;
 
 class DroneServiceTest {
 
+	@Autowired
+	private DroneService sut;
+
+	@Autowired
+	private DroneRepository droneRepo;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void createDrone_sauveLeDroneDansLeRepository() throws Exception {
+		
+		Drone drone = new Drone("nom drone 1", 1, 2, 3, Etat.ARRET);
+
+		sut.create(drone);
+		Optional<Drone> resultat = droneRepo.findById(drone.getId());
+		assertTrue(!resultat.isPresent());
+		droneRepo.save(drone);
 	}
 
+	@Test
+	void verifieListeDroneNonVideApresAjout() throws Exception {
+		
+	}
 }
